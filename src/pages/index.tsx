0e471ac15867ed718222566752b2b61a48414853
unknown <santoserick@gmail.com>
2021-04-21 12:16:54 -0300

    Antes do retorno dos dados da api, foi criado um novo objeto dos dados da api com algumas propriedades do array de objetos da api formatados, para que o componente ja receba os dados ja modificados para serem consumidos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
 import { GetStaticProps } from 'next';
+import { format, parseISO } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 import { api } from '../services/api';
+import { convertDurationTotimeString } from '../utils/convertDurationToTimeString';
 
 type Episode = {
   id: string;
   title: string;
+  thumbnail: string;
   members: string;
+  publishedAt: string;
+  durationAsString: string;
+  duration: number;
+  description: string;
+  url: string;
 };
 
 type HomeProps = {
@@ -12,20 +21,39 @@ type HomeProps = {
 };
 
 export default function Home(props: HomeProps) {
-  console.log(props.episodes);
-
   return <p>{JSON.stringify(props.episodes)}</p>;
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const response = await api.get(
-    'episodes?_limit=12&_sort=published_at&_order=desc'
-  );
-  const data = response.data;
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc',
+    },
+  });
+
+  const episodes = data.map(episode => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', {
+        locale: ptBR,
+      }),
+      durationAsString: convertDurationTotimeString(
+        Number(episode.file.duration)
+      ),
+      duration: Number(episode.file.duration),
+      description: episode.description,
+      url: episode.file.url,
+    };
+  });
 
   return {
     props: {
-      episodes: data,
+      episodes,
     },
     revalidate: 60 * 60 * 8,
   };
