3bcab3d5bac140df0c03170aad6a0622ce9a98c3
unknown <santoserick@gmail.com>
2021-04-22 22:18:51 -0300

    importado o contexto criado para englobar todos os componentes para que possam ter acesso aos dados do value do contexto

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,31 @@
+import React from 'react';
+
 import { Header } from '../Components/Header';
 import { Player } from '../Components/Player';
 
 import '../styles/global.scss';
 import styles from '../styles/app.module.scss';
+import { PlayerContext } from '../Contexts/PlayerContext';
 
 function MyApp({ Component, pageProps }) {
+  const [episodeList, setEpisodeList] = React.useState([]);
+  const [currentEpisodeIndex, setCurrentEpisodeIndex] = React.useState(0);
+
+  function play(episode) {
+    setEpisodeList([episode]);
+    setCurrentEpisodeIndex(0);
+  }
+
   return (
-    <div className={styles.wrapper}>
-      <main>
-        <Header />
-        <Component {...pageProps} />
-      </main>
-      <Player />
-    </div>
+    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play }}>
+      <div className={styles.wrapper}>
+        <main>
+          <Header />
+          <Component {...pageProps} />
+        </main>
+        <Player />
+      </div>
+    </PlayerContext.Provider>
   );
 }
 
