839b1dabd30930beafac95d8cb11656ae5088406
unknown <santoserick@gmail.com>
2021-04-22 22:13:51 -0300

    criado o contexto para compartilhar os dados do podcast clicado para o componente de Player

---
+++ src/Contexts/PlayerContext.ts
@@ -0,0 +1,17 @@
+import React from 'react';
+
+type Episode = {
+  title: string;
+  members: string;
+  thumbnail: string;
+  duration: number;
+  url: string;
+};
+
+type PlayerContextData = {
+  episodeList: Episode[];
+  currentEpisodeIndex: number;
+  play: (episode: Episode) => void;
+};
+
+export const PlayerContext = React.createContext({} as PlayerContextData);
