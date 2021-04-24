4e1a5e2a26d37fdaceab58f83910787b38271e6d
Erick <santoserick9@gmail.com>
2021-04-24 15:18:24 -0300

    criada duas expressões para verificar se existe episodio anterior e proximo, adicionada as expressoes no retorno do contexto e na tipagem

--- src/Contexts/PlayerContext.tsx
+++ src/Contexts/PlayerContext.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 type Episode = {
   title: string;
@@ -18,6 +18,8 @@ type PlayerContextData = {
   playList: (list: Episode[], index: number) => void;
   togglePlay: () => void;
   setPlayingState: (state: boolean) => void;
+  hasNext: boolean;
+  hasPrevious: boolean;
 };
 
 type PlayerContextProviderProps = {
@@ -53,16 +55,17 @@ export function PlayerContextProvider({
     setIsPlaying(state);
   }
 
-  function playNext() {
-    const nextEpisodeIndex = currentEpisodeIndex + 1;
+  const hasPrevious = currentEpisodeIndex > 0;
+  const hasNext = currentEpisodeIndex + 1 < episodeList.length;
 
-    if (nextEpisodeIndex < episodeList.length) {
+  function playNext() {
+    if (hasNext) {
       setCurrentEpisodeIndex(currentEpisodeIndex + 1);
     }
   }
 
   function playPrevious() {
-    if (currentEpisodeIndex > 0) {
+    if (hasPrevious) {
       setCurrentEpisodeIndex(currentEpisodeIndex - 1);
     }
   }
@@ -79,9 +82,15 @@ export function PlayerContextProvider({
         isPlaying,
         togglePlay,
         setPlayingState,
+        hasNext,
+        hasPrevious,
       }}
     >
       {children}
     </PlayerContext.Provider>
   );
 }
+
+export const usePlayer = () => {
+  return React.useContext(PlayerContext);
+};
