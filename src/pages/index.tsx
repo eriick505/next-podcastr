0a3532a48ba74aebe9d17788a2cf27d1f5817b73
Erick <santoserick9@gmail.com>
2021-04-24 18:29:53 -0300

    importação do componente head do next

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { GetStaticProps } from "next";
 import Image from "next/image";
 import Link from "next/link";
+import Head from "next/head";
 
 import { format, parseISO } from "date-fns";
 import ptBR from "date-fns/locale/pt-BR";
@@ -35,6 +36,9 @@ export default function Home({ latesEpisodes, allEpisodes }: HomeProps) {
 
   return (
     <div className={styles.homePage}>
+      <Head>
+        <title>Home | Podcastr</title>
+      </Head>
       <section className={styles.latesEpisodes}>
         <h2>Ultimos lançamentos</h2>
 
